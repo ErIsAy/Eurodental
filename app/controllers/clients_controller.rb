77335c2f4a0cdeea82eb0f209f82feee5ef95341
Maro Grullon <mitgrullon@hotmail.com>
2016-03-19 04:09:11 -0400

    clients and orders styling update

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -43,7 +43,8 @@ class ClientsController < ApplicationController
   def update
     respond_to do |format|
       if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Cliente fue actualizado exitosamente.' }
+        # format.html { redirect_to @client, notice: 'Cliente fue actualizado exitosamente.' }
+        format.html { redirect_to clients_url, notice: 'Cliente fue actualizado exitosamente.' }
         format.json { render :show, status: :ok, location: @client }
       else
         format.html { render :edit }
