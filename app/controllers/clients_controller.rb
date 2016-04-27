b1e38b1966c5831a86201ebbf67dd1c4f3631baf
Maro Grullon <mitgrullon@hotmail.com>
2016-04-27 01:04:39 -0400

    replaced tables in orders view

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: clients
-#
-#  id         :integer          not null, primary key
-#  name       :string
-#  phone      :string
-#  email      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
