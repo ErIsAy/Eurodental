68c50a32277523060821f46460a4c9d4282eecf5
Mario Grullon <mitgrullon@hotmail.com>
2016-07-25 00:27:29 -0400

    Added tooth db attributes

--- db/migrate/20160316023515_create_orders.rb
+++ db/migrate/20160316023515_create_orders.rb
@@ -14,9 +14,40 @@ class CreateOrders < ActiveRecord::Migration
       t.boolean  :mordida
       t.string :state, default: "En proceso"
       t.integer :quantity, default: 1
-      #t.integer :state, :default => 0
-        # pendiente = 0
-        # procesada = 1
+
+      #Tooth list attributes
+      t.integer	:t1,	default: 0
+      t.integer	:t2,	default: 0
+      t.integer	:t3,	default: 0
+      t.integer	:t4,	default: 0
+      t.integer	:t5,	default: 0
+      t.integer	:t6,	default: 0
+      t.integer	:t7,	default: 0
+      t.integer	:t8,	default: 0
+      t.integer	:t9,	default: 0
+      t.integer	:t10,	default: 0
+      t.integer	:t11,	default: 0
+      t.integer	:t12,	default: 0
+      t.integer	:t13,	default: 0
+      t.integer	:t14,	default: 0
+      t.integer	:t15,	default: 0
+      t.integer	:t16,	default: 0
+      t.integer	:t17,	default: 0
+      t.integer	:t18,	default: 0
+      t.integer	:t19,	default: 0
+      t.integer	:t20,	default: 0
+      t.integer	:t21,	default: 0
+      t.integer	:t22,	default: 0
+      t.integer	:t23,	default: 0
+      t.integer	:t24,	default: 0
+      t.integer	:t25,	default: 0
+      t.integer	:t26,	default: 0
+      t.integer	:t27,	default: 0
+      t.integer	:t28,	default: 0
+      t.integer	:t29,	default: 0
+      t.integer	:t30,	default: 0
+      t.integer	:t31,	default: 0
+      t.integer	:t32,	default: 0
 
 
       t.references :client, index: true, foreign_key: true
