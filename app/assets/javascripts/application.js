a6e720f8d9e163266bb286d0d3c921dc8f28b5e5
Mario Grullon <mitgrullon@hotmail.com>
2016-12-02 00:18:07 -0400

    testing js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -218,13 +218,12 @@ function printpage(){
   }
 
 
-  $.noConflict();
   $( document ).ready(function(){
 
-    $('.modal-trigger').leanModal();
-    $('.collapsible').collapsible({
-    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-        });
+  
+  $('.collapsible').collapsible({
+  accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      });
 
   $(function() {
       Materialize.updateTextFields();
