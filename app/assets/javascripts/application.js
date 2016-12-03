c51f757014208dfafe2fa8c7a3c03db543b9d28c
Mario Grullon <mitgrullon@hotmail.com>
2016-12-03 09:27:17 -0400

    fixed a few pending changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -154,150 +154,150 @@ $(document).on("change", "select#procedure_store_id", function(e){
 
 
 /*JS pending refactoring*/
-
-(function() {
-    // $.mask.definitions['~']='[+-]';
-    //$(".money-mask").mask("9? 99? 9999",{placeholder:" "});
-    $(".phone-mask").mask("(999) 999-9999");
-    $("#e1").select2();
-    $('#e1').select2({
-        language: {
-            noResults: function() {
-                return "Cliente no encontrado.  <a href='/clients/new'>Desea crearlo?</a>";
-            }
-        },
-        escapeMarkup: function(markup) {
-            return markup;
-        }
-    });
-})();
-
-(function() {
-  $('.datepicker').pickadate({
-        format: 'yyyy-mm-dd',
-        //set today the min date
-        min: true,
-        // Strings and translations
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year
-        monthsShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-        monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        weekdaysShort: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
-        weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-
-        // Buttons
-        today: 'Hoy',
-        clear: 'Borrar',
-        close: 'X',
-
-        // Accessibility labels
-        labelMonthNext: 'Siguiente Mes',
-        labelMonthPrev: 'Mes Anterior',
-        labelMonthSelect: 'Seleccionar Mes',
-        labelYearSelect: 'Seleccionar Año'
-
-
-
-      });
-})();
-
-
-function printpage(){
-     window.print()
-  }
-
-  $(document).ready(function() {
-    // Materialize.updateTextFields();
-    $('.modal-trigger').leanModal();
-    //$('.modal').modal();
-    // $('.modal-trigger').leanmodal();
-  });
-
-
-  $(document).ready(function(){
-
-      $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-          });
-  });
-
-
-
-  $( document ).ready(function(){
-  //
-  $(function() {
-      Materialize.updateTextFields();
-  });
-
-  //$('.modal-trigger').leanModal();
-
-  // $(function() {
-  //
-  // });
-  $(".button-collapse").sideNav();
-  $('select').material_select();
-  $('.tooltipped').tooltip({delay: 25});
-  $('ul.tabs').tabs();
-  $('input#input_text, textarea#textarea1').characterCounter();
-
-
-  //Datepicker withou date restriction
-  $('.datepicker_all').pickadate({
-    format: 'yyyy-mm-dd',
-    // Strings and translations
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    monthsShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-    monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    weekdaysShort: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
-    weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-
-    // Buttons
-    today: 'Hoy',
-    clear: 'Borrar',
-    close: 'Cerrar',
-
-    // Accessibility labels
-    labelMonthNext: 'Siguiente Mes',
-    labelMonthPrev: 'Mes Anterior',
-    labelMonthSelect: 'Seleccionar Mes',
-    labelYearSelect: 'Seleccionar Año'
-
-
-
-  });
-  $(".tabfactura").click(function() {
-    document.getElementById("invoice_tab").className = document.getElementById("invoice_tab").className.replace(/\bdisabled\b/,'');
-    $('ul.tabs').tabs('select_tab', 'test2');
-
-  });
-  $(".tabfactura_back").click(function() {
-    $('ul.tabs').tabs('select_tab', 'test1');
-
-  });
-
-
-
-  /*Changes Order Date to disabled and clear date if exist*/
-  $("#sale_coti").click(function() {
-            if (this.checked == true) {
-              document.getElementById("sale_order_date").disabled = "disabled";
-              document.getElementById("sale_order_date").value = "";
-            }
-            else{
-              document.getElementById("sale_order_date").disabled = "";
-             }
-      });
-
-    $(function(){
-        $('a[data-reload="true"]').on('click', function(e) {
-
-            var delay=1000;
-            setTimeout(function() {
-                window.location = $(e.target).attr('href');
-                window.location.reload(true);
-            }, delay);
-
-        });
-    });
-});
+//
+// (function() {
+//     // $.mask.definitions['~']='[+-]';
+//     //$(".money-mask").mask("9? 99? 9999",{placeholder:" "});
+//     $(".phone-mask").mask("(999) 999-9999");
+//     $("#e1").select2();
+//     $('#e1').select2({
+//         language: {
+//             noResults: function() {
+//                 return "Cliente no encontrado.  <a href='/clients/new'>Desea crearlo?</a>";
+//             }
+//         },
+//         escapeMarkup: function(markup) {
+//             return markup;
+//         }
+//     });
+// })();
+//
+// (function() {
+//   $('.datepicker').pickadate({
+//         format: 'yyyy-mm-dd',
+//         //set today the min date
+//         min: true,
+//         // Strings and translations
+//         selectMonths: true, // Creates a dropdown to control month
+//         selectYears: 15, // Creates a dropdown of 15 years to control year
+//         monthsShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+//         monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+//         weekdaysShort: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+//         weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+//
+//         // Buttons
+//         today: 'Hoy',
+//         clear: 'Borrar',
+//         close: 'X',
+//
+//         // Accessibility labels
+//         labelMonthNext: 'Siguiente Mes',
+//         labelMonthPrev: 'Mes Anterior',
+//         labelMonthSelect: 'Seleccionar Mes',
+//         labelYearSelect: 'Seleccionar Año'
+//
+//
+//
+//       });
+// })();
+//
+//
+// function printpage(){
+//      window.print()
+//   }
+//
+//   $(document).ready(function() {
+//     // Materialize.updateTextFields();
+//     $('.modal-trigger').leanModal();
+//     //$('.modal').modal();
+//     // $('.modal-trigger').leanmodal();
+//   });
+//
+//
+//   $(document).ready(function(){
+//
+//       $('.collapsible').collapsible({
+//       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+//           });
+//   });
+//
+//
+//
+//   $( document ).ready(function(){
+//   //
+//   $(function() {
+//       Materialize.updateTextFields();
+//   });
+//
+//   //$('.modal-trigger').leanModal();
+//
+//   // $(function() {
+//   //
+//   // });
+//   $(".button-collapse").sideNav();
+//   $('select').material_select();
+//   $('.tooltipped').tooltip({delay: 25});
+//   $('ul.tabs').tabs();
+//   $('input#input_text, textarea#textarea1').characterCounter();
+//
+//
+//   //Datepicker withou date restriction
+//   $('.datepicker_all').pickadate({
+//     format: 'yyyy-mm-dd',
+//     // Strings and translations
+//     selectMonths: true, // Creates a dropdown to control month
+//     selectYears: 15, // Creates a dropdown of 15 years to control year
+//     monthsShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+//     monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+//     weekdaysShort: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+//     weekdaysFull: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+//
+//     // Buttons
+//     today: 'Hoy',
+//     clear: 'Borrar',
+//     close: 'Cerrar',
+//
+//     // Accessibility labels
+//     labelMonthNext: 'Siguiente Mes',
+//     labelMonthPrev: 'Mes Anterior',
+//     labelMonthSelect: 'Seleccionar Mes',
+//     labelYearSelect: 'Seleccionar Año'
+//
+//
+//
+//   });
+//   $(".tabfactura").click(function() {
+//     document.getElementById("invoice_tab").className = document.getElementById("invoice_tab").className.replace(/\bdisabled\b/,'');
+//     $('ul.tabs').tabs('select_tab', 'test2');
+//
+//   });
+//   $(".tabfactura_back").click(function() {
+//     $('ul.tabs').tabs('select_tab', 'test1');
+//
+//   });
+//
+//
+//
+//   /*Changes Order Date to disabled and clear date if exist*/
+//   $("#sale_coti").click(function() {
+//             if (this.checked == true) {
+//               document.getElementById("sale_order_date").disabled = "disabled";
+//               document.getElementById("sale_order_date").value = "";
+//             }
+//             else{
+//               document.getElementById("sale_order_date").disabled = "";
+//              }
+//       });
+//
+//     $(function(){
+//         $('a[data-reload="true"]').on('click', function(e) {
+//
+//             var delay=1000;
+//             setTimeout(function() {
+//                 window.location = $(e.target).attr('href');
+//                 window.location.reload(true);
+//             }, delay);
+//
+//         });
+//     });
+// });
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,15 +3,21 @@ class ReportsController < ApplicationController
   end
 
   def orders_unpaid
-    @sales = Sale.all
+    # @sales = Sale.all
+    @search = Sale.ransack(params[:q])
+    @sales = @search.result.page(params[:page]).per_page(15)
   end
 
   def orders_paid
-    @sales = Sale.all
+    # @sales = Sale.all
+    @search = Sale.ransack(params[:q])
+    @sales = @search.result.page(params[:page]).per_page(15)
   end
 
   def pending_balance
-    @sales = Sale.all
-    @clients = Client.all
+    # @sales = Sale.all
+    # @clients = Client.all
+    @search = Sale.ransack(params[:q])
+    @sales = @search.result.page(params[:page]).per_page(15)
   end
 end
