20ca3222a1f13e8783a0543a7ea90cb91c2b30d5
Mario Grullon <mitgrullon@hotmail.com>
2016-12-08 15:39:38 -0400

    updated seeds for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,13 @@ User.create(email: 'admin@admin.com', password: '123456789', role_level: 1, admi
 # phones = ClientPhone.new :phone => Faker::PhoneNumber.phone_number,
 #                              :ptype => "Movil"
 
-
-50.times do
-  Client.create(name: Faker::Name.name,
-                address: Faker::Address.street_address,
-                :client_phones_attributes => [:phone => Faker::PhoneNumber.phone_number,
-                                             :ptype => "Movil"])
-end
+# 
+# 50.times do
+#   Client.create(name: Faker::Name.name,
+#                 address: Faker::Address.street_address,
+#                 :client_phones_attributes => [:phone => Faker::PhoneNumber.phone_number,
+#                                              :ptype => "Movil"])
+# end
 
 Price.create
 
@@ -44,59 +44,59 @@ Price.create
 #                 { name: 'Metales'}])
 #
 # # Tipo de Trabajo - Items
-Worktype.create([{ name: 'Corona', price:1},
-                { name: 'Puente', price:1},
-                { name: 'Implantes', price:1},
-                { name: 'Barra', price:1},
-                { name: 'Híbrida', price:1},
-                { name: 'Carilla', price:1},
-                { name: 'Inlay/Onlay' , price:1},
-                { name: 'Inlay Bridge' , price:1},
-                { name: 'Lente de contacto', price:1 },
-                { name: 'Perno', price:1},
-                { name: 'Soldadura Láser', price:1 },
-                { name: 'Abutment Personalizado', price:1}])
+Worktype.create([{ name: 'Corona', price:0},
+                { name: 'Puente', price:0},
+                { name: 'Implantes', price:0},
+                { name: 'Barra', price:0},
+                { name: 'Híbrida', price:0},
+                { name: 'Carilla', price:0},
+                { name: 'Inlay/Onlay' , price:0},
+                { name: 'Inlay Bridge' , price:0},
+                { name: 'Lente de contacto', price:0 },
+                { name: 'Perno', price:0},
+                { name: 'Soldadura Láser', price:0 },
+                { name: 'Abutment Personalizado', price:0}])
 #
 #
 # # Material - Items
-Material.create([{ name: 'Zirconio', price:1 },
-                { name: 'Disilicato', price:1 },
-                { name: 'Fedespática', price:1 },
-                { name: 'Porcelana', price:1 },
-                { name: 'PMMA', price:1 },
-                { name: 'Polímero', price:1},
-                { name: 'Composite', price:1},
-                { name: 'Metal/Pocelana', price:1},
-                { name: 'Zirconio/Pocelana', price:1 },
-                { name: 'Peek', price:1 },
-                { name: 'Cromo cobalto', price:1 },
-                { name: 'Titanium', price:1 },
-                { name: 'Cromo níquel', price:1 },
-                { name: 'Oro', price:1 },
-                { name: 'Paladio', price:1 }])
+Material.create([{ name: 'Zirconio', price:0 },
+                { name: 'Disilicato', price:0 },
+                { name: 'Fedespática', price:0 },
+                { name: 'Porcelana', price:0 },
+                { name: 'PMMA', price:0 },
+                { name: 'Polímero', price:0},
+                { name: 'Composite', price:0},
+                { name: 'Metal/Pocelana', price:0},
+                { name: 'Zirconio/Pocelana', price:0 },
+                { name: 'Peek', price:0 },
+                { name: 'Cromo cobalto', price:0 },
+                { name: 'Titanium', price:0 },
+                { name: 'Cromo níquel', price:0 },
+                { name: 'Oro', price:0 },
+                { name: 'Paladio', price:0 }])
 
 
 # # Color de muñon - Items
-Mcolor.create([{ name: 'Natural', price:1},
-                { name: 'Pigmentado', price:1},
-                { name: 'Metal', price:1}])
+Mcolor.create([{ name: 'Natural', price:0},
+                { name: 'Pigmentado', price:0},
+                { name: 'Metal', price:0}])
 
 # # Colorimetro - Items
-Gcolor.create([{ name: 'Vita Classic', price:1},
-                { name: 'Vita 3D master', price:1},
-                { name: 'Bioform', price:1},
-                { name: 'Chromarcop', price:1 },
-                { name: 'Otras', price:1}])
+Gcolor.create([{ name: 'Vita Classic', price:0},
+                { name: 'Vita 3D master', price:0},
+                { name: 'Bioform', price:0},
+                { name: 'Chromarcop', price:0 },
+                { name: 'Otras', price:0}])
 
 
 # # Procedimiento - Items
-Procedure.create([{ name: 'Fresado/Maquinado', price:1},
-                { name: 'Prensado', price:1},
-                { name: 'Colado', price:1 }])
+Procedure.create([{ name: 'Fresado/Maquinado', price:0},
+                { name: 'Prensado', price:0},
+                { name: 'Colado', price:0 }])
 
 # # Metales - Items
-# Item.create([{ name: 'Cromo cobalto', price:1, category_id:6 },
-#                 { name: 'Titanium', price:1, category_id:6 },
-#                 { name: 'Cromo níquel', price:1, category_id:6 },
-#                 { name: 'Oro', price:1, category_id:6 },
-#                 { name: 'Paladio', price:1, category_id:6 }])
+# Item.create([{ name: 'Cromo cobalto', price:0, category_id:6 },
+#                 { name: 'Titanium', price:0, category_id:6 },
+#                 { name: 'Cromo níquel', price:0, category_id:6 },
+#                 { name: 'Oro', price:0, category_id:6 },
+#                 { name: 'Paladio', price:0, category_id:6 }])
