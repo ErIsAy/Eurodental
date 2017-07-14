e24c11305a8a146e28bff84604e358d0b431e5d9
Mario Grullon <mitgrullon@hotmail.com>
2017-07-13 21:45:18 -0400

    Updated locales and added patient to index pages

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module EuroDentalLab
     # config.i18n.default_locale = :de
 
      config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+     config.i18n.available_locales = [:es, :en]
      config.i18n.default_locale = :es
      config.serve_static_files = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
