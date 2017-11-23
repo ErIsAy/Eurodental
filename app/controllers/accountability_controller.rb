8ddcf89eddfc77d98956bcb8330b2dae76225f95
Mario Grullon <mitgrullon@hotmail.com>
2017-11-23 18:58:25 -0400

    removed circle icons , font changes, sequence in consults

--- app/controllers/accountability_controller.rb
+++ app/controllers/accountability_controller.rb
@@ -1,6 +1,9 @@
 class AccountabilityController < ApplicationController
   def index
-    @search = Sale.ransack(params[:q])
-    @sales = @search.result
+    # @search = Sale.ransack(params[:q])
+    # @sales = @search.result
+
+    @search = Sale.joins(:invoice_number).merge(InvoiceNumber.order(id: :asc)).ransack(params[:q])
+    @sales = @search.result.page(params[:page])
   end
 end
